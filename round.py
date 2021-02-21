Fixed a faulty formula.
I tried to weigh the probability of getting an answer with the number of questions on the board, but it ended up being too lopsided. I've since decided to keep the probability of getting an answer right independent of the number of questions on the board. This seems to line up with what I've seen from watching Family Feud. This may seem mathematically odd, but the odds of getting an answer isn't actually random (since they can adjust the answer they give based on the answers on the board as well as consiously work out what the answer is likely to be), so it's not a bad approximation.

--Matteo Raso

round.py
@@ -15,7 +15,7 @@ def round(num_questions, odds_of_answering):
     num_questions -= 1
     while (num_strikes < 3) and (num_questions > 0):
         #More questions on the board means better chances.
-        if (np.random.random() * num_questions) > (1 - odds_of_answering):
+        if np.random.random() < odds_of_answering:
             A_score += question_value
             num_questions -= 1
 
@@ -24,7 +24,7 @@ def round(num_questions, odds_of_answering):
 
     #Family B goes in for the steal.
     if num_questions > 0:
-        if (np.random.random() * num_questions) > (1 - odds_of_answering):
+        if np.random.random() < odds_of_answering:
             B_score += (question_value + A_score)
             A_score = 0
 